1d9cb6f6e40274310e5dcaefb003b26ea60ae6fd
joao-zanetti <57362590+joao-zanetti@users.noreply.github.com>
2020-01-15 15:44:15 -0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace Navigater
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        EditText editText1;
+        EditText editText2;
+        Button buttonGo;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            editText1 = FindViewById<EditText>(Resource.Id.editText1);
+            editText2 = FindViewById<EditText>(Resource.Id.editText2);
+            buttonGo = FindViewById<Button>(Resource.Id.buttonGo);
+            buttonGo.Click += (s, e) =>
+            {
+                Intent nextActivity = new Intent(this, typeof(RecvActivity));
+                nextActivity.PutExtra("name", editText1.Text);
+                nextActivity.PutExtra("email", editText2.Text);
+                StartActivity(nextActivity);
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ RecvActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Navigater
+{
+    [Activity(Label = "RecvActivity")]
+    public class RecvActivity : Activity
+    {
+        TextView textView1;
+        TextView textView2;
+        Button buttonBack;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.recvactivity);
+
+            textView1 = FindViewById<TextView>(Resource.Id.textView1);
+            textView2 = FindViewById<TextView>(Resource.Id.textView2);
+            buttonBack = FindViewById<Button>(Resource.Id.buttonBack);
+
+            string text1 = Intent.GetStringExtra("name" ?? "Not rec");
+            string text2 = Intent.GetStringExtra("email" ?? "Not rec");
+
+            textView1.Text = text1;
+            textView2.Text = text2;
+
+            buttonBack.Click += delegate
+            {
+                this.Finish();
+            };
+
+        }
+    }
+}
